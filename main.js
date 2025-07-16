99938e9d0457d0372c0735a39d936e4bafea6c39
Mazuki <mazuki.wu@gmail.com>
2025-07-16 15:33:45 -0500

    Add functionality to change grid squares
    On button click, the user can now change the number
    of squares the grid contains. This helps the website
    respond more dynamically to the user's needs.

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const container = document.querySelector(".grid");
+const grid = document.querySelector(".grid");
 const button = document.querySelector("button");
+const body = document.querySelector("button");
 
 function createGrid(squares) {
     for (let i = 0; i < squares; i++) {
         boxContainer = document.createElement("div");
         boxContainer.classList.add("boxContainer");
-        container.appendChild(boxContainer);
+        grid.appendChild(boxContainer);
         for (let i = 0; i < squares; i++) {
             box = document.createElement("div");
             box.classList.add("box");
@@ -21,7 +22,20 @@ function newBgColor(event) {
 function resetBgColor(event) {
     event.target.style.backgroundColor = "transparent";
 }
+
+function resetGrid() {
+    grid.innerHTML = '';
+}
+
 createGrid(4);
-container.addEventListener("mouseover", newBgColor);
-container.addEventListener("mouseout", resetBgColor);
+grid.addEventListener("mouseover", newBgColor);
+grid.addEventListener("mouseout", resetBgColor);
+button.addEventListener("click", () => {
+    let squares = prompt("Number of squares per side? (limit of 100)");
+    while (squares > 100) {
+        squares = prompt("Invalid input. Please enter the number of squares per side again (limit of 100)");
+    }
+    resetGrid();
+    createGrid(squares);
+})
 
