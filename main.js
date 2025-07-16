947e5fa63b6ef183aaf7d903cc5e4ffc10489532
Mazuki <mazuki.wu@gmail.com>
2025-07-16 14:43:30 -0500

    Create 4 boxes in each row of grid.
    Boxes were made to take up available space of each
    row evenly. They were additionally styled basically
    so they could be told apart clearly.

--- main.js
+++ main.js
@@ -3,4 +3,9 @@ for (let i = 0; i < 4; i++) {
     boxContainer = document.createElement("div");
     boxContainer.classList.add("boxContainer");
     container.appendChild(boxContainer);
+    for (let i = 0; i < 4; i++) {
+        box = document.createElement("div");
+        box.classList.add("box");
+        boxContainer.appendChild(box);
+    }
 }
